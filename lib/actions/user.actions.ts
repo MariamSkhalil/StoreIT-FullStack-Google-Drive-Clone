f1e0fed3bf8ef2ac43a027bba7bc7e097db61131
Mariam Khalil <mariemalmasry@gmail.com>
2025-09-01 00:23:40 +0300

    update

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -103,18 +103,22 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  const { databases, account } = await createSessionClient();
+  try {
+    const { databases, account } = await createSessionClient();
 
-  const result = await account.get();
-  const user = await databases.listDocuments(
-    appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId,
-    [Query.equal("accountId", result.$id)],
-  );
+    const result = await account.get();
+    const user = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal("accountId", result.$id)],
+    );
 
-  if (user.total <= 0) return null;
+    if (user.total <= 0) return null;
 
-  return parseStringfy(user.documents[0]);
+    return parseStringfy(user.documents[0]);
+  } catch (error) {
+    handleError(error, "Failed to get current user");
+  }
 };
 
 export const logoutUser = async () => {
